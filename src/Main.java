e5ff27d8d88e09fdf317f25bb5df1d2798bb8e13
ZINEBMOKHLIS <zinebmokhlis15@gmail.com>
2023-03-14 13:11:56 +0000

    first ver

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    public int a;
+    public int transform(int a){
+        return a=3+4;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main mfc=new Main();
+        mfc.a=mfc.transform(7);
+        mfc.a=mfc.transform(mfc.a);
+        System.out.println("transforming a !"+mfc.a );
     }
 }
